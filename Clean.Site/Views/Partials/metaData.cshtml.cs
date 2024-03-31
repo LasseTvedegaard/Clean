6e9e9ea6026ef00882b4840400c3bc9bbf05df86
LasseTN <103989770+LasseTN@users.noreply.github.com>
2024-03-31 12:30:46 +0200

    Add project files.

---
+++ Clean.Site/Views/Partials/metaData.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Clean.Site.Views.Partials
+{
+    public class metaDataModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
